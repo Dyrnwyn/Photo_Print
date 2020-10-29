919bdbd0c17f1e119f873e0fc5de1747a1fa2d94
dyrnwyn <pod1987@gmail.com>
2020-10-29 10:06:54 +0700

    many fixes

--- source/combinator.py
+++ source/combinator.py
@@ -1,5 +1,9 @@
 import os
 import re
+from psd_tools import PSDImage
+from PIL import ImageFilter
+from PyQt5 import QtCore
+
 
 def search_file(flExt, fldr=""):
     # Во временно директории ищем все файлы c переданным разрешением
@@ -11,3 +15,148 @@ def search_file(flExt, fldr=""):
                     re.findall(r"^.*\." + flExt + "*", fl.name):
                 filteredFlList.append(fl.name)
     return filteredFlList
+
+
+class ThreadForConvert(QtCore.QThread):
+    ratio_dict = {"п_10х15_": 1.5,
+                  "п_15х20_": 1.333,
+                  "п_20х30_": 1.5,
+                  "п_Настенный календарь_": 1.3778,
+                  "п_магнит_": 1.428,
+                  "п_магнит 10х15_": 1.5,
+                  "о_10х15_": 1.5,
+                  "о_15х20_": 1.333,
+                  "о_20х30_": 1.5,
+                  "о_Настенный календарь_": 1.3778,
+                  "о_магнит 10х15_": 1.5,
+                  "о_календарик 7х10_": 1.428,
+                  "_Брелок 58_": 0.32,
+                  "_Зеркало 75_": 0.4,
+                  "_Значок 75_": 0.4,
+                  "_Значок 100_": 0.518,
+                  "_Значок 158_": 0.84,
+                  "_Копилка 158_": 0.84,
+                  "_Кружка-термос с крышкой_": 0}
+
+    dict_of_sizes = {"п_10х15_": (1772, 1181),
+                     "п_15х20_": (2362, 1772),
+                     "п_20х30_": (3543, 2362),
+                     "п_Настенный календарь_": (3661, 2657),
+                     "п_магнит_": (1181, 827),
+                     "п_магнит 10х15_": (1772, 1181),
+                     "о_10х15_": (1772, 1181),
+                     "о_15х20_": (2362, 1772),
+                     "о_20х30_": (3543, 2362),
+                     "о_Настенный календарь_": (3661, 2657),
+                     "о_магнит 10х15_": (1772, 1181),
+                     "о_календарик 7х10_": (1181, 827),
+                     "_Брелок 58_": 0.32,
+                     "_Зеркало 75_": 0.4,
+                     "_Значок 75_": 0.4,
+                     "_Значок 100_": 0.518,
+                     "_Значок 158_": 0.84,
+                     "_Копилка 158_": 0.84,
+                     "_Кружка-термос с крышкой_": 0}
+    count_converted_photo = QtCore.pyqtSignal(int)
+    # список имен плосских фотографий
+    flat_photo_name_list = ("п_10х15_", "п_15х20_", "п_20х30_", "п_магнит_", "п_магнит 10х15_")
+    # список имен плосских фотографий
+    stereo_photo_name_list = ("о_10х15_", "о_15х20_", "о_20х30_", "о_магнит 10х15_", "о_календарик 7х10_")
+    # список имен плосских фотографий
+    wall_calendar_name_list = ("п_Настенный календарь_", "о_Настенный календарь_")
+    badge_name_list = ("_Брелок 58_", "_Зеркало 75_", "_Значок 75_", "_Значок 100_", "_Значок 158_", "_Копилка 158_")
+    cup_name_list = ("Кружка-термос с крышкой")
+    dpi = (300, 300)
+    dict_of_psd_files = {}
+    dir_for_png = 'C:\\Объекты\\print_photo_png'
+
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    def set_var(self, dpi, psd_dict, dir_for_png):
+        """Устанавливаем значения переменных
+        разрешение фотографии dpi, словарь psd файлов, путь к папке для еконвертации в png"""
+        self.photo_dpi = dpi
+        self.dict_of_psd_files = psd_dict
+        self.dir_for_png = dir_for_png
+
+    def crop_image(self, img, ratio):
+        if img.width > img.height:   # опеределяем горизонтальную фотографию
+            new_width = round(img.height * ratio)
+            left = round((img.width - new_width) / 2)
+            upper = 0
+            right = img.width - left
+            lower = img.height
+        elif img.width < img.height:  # опеределяем вертикальную фотографию
+            new_width = round(img.height / ratio)
+            left = round((img.width - new_width) / 2)
+            upper = 0
+            right = img.width - left
+            lower = img.height
+        print((left, upper, right, lower))
+        return img.crop((left, upper, right, lower))
+
+    def resize_image(self, img, photo_format):
+        if img.width > img.height:   # опеределяем горизонтальную фотографию
+            new_width = self.dict_of_sizes[photo_format][0]
+            new_height = self.dict_of_sizes[photo_format][1]
+        elif img.width < img.height:  # опеределяем вертикальную фотографию
+            new_width = self.dict_of_sizes[photo_format][1]
+            new_height = self.dict_of_sizes[photo_format][0]
+        return img.resize((new_width, new_height))
+
+    def run(self):
+        count = 0
+        for k, v in self.dict_of_psd_files.items():
+            path_for_png = self.dir_for_png + '\\' + os.path.split(k)[1]
+            for file in v:
+                psd_file = PSDImage.open(k + '\\' + file)
+                img = psd_file.composite()
+                photo_format = file.split("_")[0] + "_" + file.split("_")[1] + "_"
+                ratio = self.get_ratio(file)
+                if photo_format in self.flat_photo_name_list:
+                    cropped_img = self.crop_image(img, ratio)
+                    img_for_save = self.resize_image(cropped_img, photo_format)
+                elif photo_format in self.stereo_photo_name_list:
+                    pass
+                elif photo_format in self.wall_calendar_name_list:
+                    cropped_img = self.crop_image(img, ratio)
+                    resized_img = self.resize_image(cropped_img, photo_format)
+                    img_for_save = self.wall_calendar_image(resized_img)
+                elif photo_format in self.cup_name_list:
+                    img_for_save = self.convert_cup_image(img, ratio)
+                elif photo_format in self.badge_name_list:
+                    img_for_save = self.convert_badge_image(img, ratio)
+                else:
+                    pass
+                try:
+                    img_for_save.save(path_for_png + '\\' + file[0: -4] + '.png', compress_level=0, dpi=self.photo_dpi)
+                except Exception:
+                    pass
+                count += 1
+                self.count_converted_photo.emit(count)
+
+    def get_ratio(self, file_name):
+        photo_size = file_name.split("_")[0] + "_" + file_name.split("_")[1] + "_"
+        return self.ratio_dict[photo_size]
+
+    def convert_photo_image(self, img, ratio):
+        pass
+
+    def wall_calendar_image(self, img):
+        """Конвертируем настенный календарь в пнг, размываем фото для нижнего слоя,
+        и вставляем сверху фото, меньшего размера, приблизительно 5-7мм рамки, нижнего слоя"""
+        front_img = img.resize((round(img.width / 1.0505), round(img.height / 1.0505))).convert('RGBA')
+        blured_img = img.filter(ImageFilter.GaussianBlur(radius=35)).convert('RGBA')
+        left = round((blured_img.width - front_img.width) / 2)
+        top = round((blured_img.height - front_img.height) / 2)
+        blured_img.alpha_composite(front_img, (left, top))
+        return blured_img
+
+    def convert_cup_image(self, img, ratio):
+        return img
+
+    def convert_badge_image(self, img, ratio):
+        new_width = round(img.width * ratio)
+        new_height = round(img.height * ratio)
+        return img.resize((new_width, new_height))
--- source/main.py
+++ source/main.py
@@ -1,130 +1,15 @@
 import sys
-from PyQt5 import QtWidgets, QtCore
-import window
-import combinator
 import os
-from psd_tools import PSDImage
-from PIL import ImageFilter
-
-
-dir_for_png = 'C:\\Объекты\\print_photo_png'
-progress_bar_maximum = 0
-dict_of_psd_files = {}
-photo_dpi = (300, 300)
-
-ratio_dict = {"п_10х15_": 1.5,
-              "п_15х20_": 1.333,
-              "п_20х30_": 1.5,
-              "п_Настенный календарь_": 1.3778,
-              "п_магнит_": 1.428,
-              "п_магнит 10х15_": 1.5,
-              "о_10х15_": 1.5,
-              "о_15х20_": 1.333,
-              "о_20х30_": 1.5,
-              "о_Настенный календарь_": 1.3778,
-              "о_магнит 10х15_": 1.5,
-              "о_календарик 7х10_": 1.428,
-              "_Брелок 58_": 0.32,
-              "_Зеркало 75_": 0.4,
-              "_Значок 75_": 0.4,
-              "_Значок 100_": 0.518,
-              "_Значок 158_": 0.84,
-              "_Копилка 158_": 0.84,
-              "_Кружка-термос с крышкой_": 0}
-
-
-class ThreadForConvert(QtCore.QThread):
-    count_converted_photo = QtCore.pyqtSignal(int)
-    # списки форматов фотографий
-    photo_name_list = ("10х15", "15х20", "20х30", "магнит", "магнит 10х15", "календарик 7х10")
-    wall_calendar_name_list = ("Настенный календарь", )
-    badge_name_list = ("Брелок 58", "Зеркало 75", "Значок 75", "Значок 100", "Значок 158", "Копилка 158")
-    cup_name_list = ("Кружка-термос с крышкой")
-
-    def __init__(self):
-        QtCore.QThread.__init__(self)
-
-    def run(self):
-        global photo_dpi
-        global dir_for_png
-        global dict_of_psd_files
-        count = 0
-        for k, v in dict_of_psd_files.items():
-            path_for_png = dir_for_png + '\\' + os.path.split(k)[1]
-            for file in v:
-                psd_file = PSDImage.open(k + '\\' + file)
-                img = psd_file.composite()
-                photo_format = file.split("_")[1]
-                ratio = self.get_ratio(file)
-                if photo_format in self.photo_name_list:
-                    img_resized = self.convert_photo_image(img, ratio)
-                elif photo_format in self.cup_name_list:
-                    img_resized = self.convert_cup_image(img, ratio)
-                elif photo_format in self.wall_calendar_name_list:
-                    img_resized = self.convert_wall_calendar_image(img, ratio)
-                elif photo_format in self.badge_name_list:
-                    img_resized = self.convert_badge_image(img, ratio)
-                else:
-                    pass
-                img_resized.save(path_for_png + '\\' + file[0: -4] + '.png', compress_level=0, dpi=photo_dpi)
-                count += 1
-                self.count_converted_photo.emit(count)
-
-    def get_ratio(self, file_name):
-        global ratio_dict
-        photo_size = file_name.split("_")[0] + "_" + file_name.split("_")[1] + "_"
-        return ratio_dict[photo_size]
-
-    def convert_photo_image(self, img, ratio):
-        if img.width > img.height:   # опеределяем горизонтальную фотографию
-            new_width = img.height * ratio
-            left = (img.width - new_width) / 2
-            upper = 0
-            right = img.width - left
-            lower = img.height
-        elif img.width < img.height:  # опеределяем вертикальную фотографию
-            new_width = img.height / ratio
-            left = (img.width - new_width) / 2
-            upper = 0
-            right = img.width - left
-            lower = img.height
-        return img.crop((left, upper, right, lower))
-
-    def convert_wall_calendar_image(self, img, ratio):
-        """Конвертируем настенный календарь в пнг, размываем фото для нижнего слоя,
-        и вставляем сверху фото, меньшего размера, приблизительно 5-7мм рамки, нижнего слоя"""
-        if img.width > img.height:   # опеределяем горизонтальную фотографию
-            new_width = img.height * ratio
-            left = (img.width - new_width) / 2
-            upper = 0
-            right = img.width - left
-            lower = img.height
-            cropped_img = img.crop((left, upper, right, lower)).resize((3661, 2657))
-        elif img.width < img.height:  # опеределяем вертикальную фотографию
-            new_width = img.height / ratio
-            left = (img.width - new_width) / 2
-            upper = 0
-            right = img.width - left
-            lower = img.height
-            cropped_img = img.crop((left, upper, right, lower)).resize((2657, 3661))
-        front_img = cropped_img.resize((round(cropped_img.width/1.0505), round(cropped_img.height/1.0505)))
-        blured_img = cropped_img.filter(ImageFilter.GaussianBlur(radius = 35))
-        left = round((blured_img.width - front_img.width)/2)
-        top = round((blured_img.height - front_img.height)/2)
-        blured_img.alpha_composite(front_img, (left, top))
-        return blured_img
-        
-
-    def convert_cup_image(self, img, ratio):
-        return img
-
-    def convert_badge_image(self, img, ratio):
-        new_width = round(img.width * ratio)
-        new_height = round(img.height * ratio)
-        return img.resize((new_width, new_height))
+import window
+from PyQt5 import QtWidgets, QtCore
+from combinator import search_file, ThreadForConvert
 
 
 class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
+    dir_for_png = 'C:\\Объекты\\print_photo_png'
+    progress_bar_maximum = 0
+    dict_of_psd_files = {}
+    photo_dpi = (300, 300)
     row_count = 0  # счетчик строк, заполненных в таблице
     count_photo = {"п_10х15_": 0,  # Словарь подсчета количества печатаемых фото
                    "п_15х20_": 0,
@@ -132,11 +17,13 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
                    "п_Настенный календарь_": 0,
                    "п_магнит_": 0,
                    "п_магнит 10х15_": 0,
-                   "Брелок 58": 0,
-                   "Зеркало 75": 0,
-                   "Значок 158": 0,
-                   "Копилка 158": 0,
-                   "Кружка-термос с крышкой": 0, }
+                   "_Брелок 58_": 0,
+                   "_Зеркало 75_": 0,
+                   "_Значок 75_": 0,
+                   "_Значок 100_": 0,
+                   "_Значок 158_": 0,
+                   "_Копилка 158_": 0,
+                   "_Кружка-термос с крышкой_": 0, }
 
     def __init__(self):
         super().__init__()
@@ -150,6 +37,11 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
         self.convert_psd_to_png_thread.started.connect(self.on_start_convert)
         self.convert_psd_to_png_thread.finished.connect(self.on_finished_convert)
         self.convert_psd_to_png_thread.count_converted_photo.connect(self.on_change_convert, QtCore.Qt.QueuedConnection)
+
+    def compose_files(self):
+        self.dict_of_psd_files = self.get_list_of_psd_from_table()
+        self.create_object_dir(self.dict_of_psd_files)
+        self.convert_psd_to_png()
 
     def on_change_convert(self, v):
         """Функция вызываемая во время процесса конвертации"""
@@ -165,7 +57,7 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
         self.composeButton.setDisabled(False)
 
     def convert_psd_to_png(self):
-        print(dict_of_psd_files)
+        self.convert_psd_to_png_thread.set_var(self.photo_dpi, self.dict_of_psd_files, self.dir_for_png)
         self.convert_psd_to_png_thread.start()
 
     def zeroing_count_photo(self):
@@ -176,24 +68,25 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
     def add_amount_of_photo_to_label(self, file_name):
         _translate = QtCore.QCoreApplication.translate
         for k, v in self.count_photo.items():   # находим в файле количество в печать
-                if k in file_name:              # и прибавляем к общему количеству данного формата фотографии
-                    amount = int(file_name.split("_")[4])
-                    v += amount
-                    self.count_photo[k] = v
+            if k in file_name:              # и прибавляем к общему количеству данного формата фотографии
+                amount = int(file_name.split("_")[4])
+                v += amount
+                self.count_photo[k] = v
         self.label_10x15.setText(_translate("MainWindow", "10x15: " + str(self.count_photo["п_10х15_"])))
         self.label_15x20.setText(_translate("MainWindow", "15x20: " + str(self.count_photo["п_15х20_"])))
         self.label_20x30.setText(_translate("MainWindow", "20x30: " + str(self.count_photo["п_20х30_"])))
         self.label_WallCalendar.setText
         (_translate("MainWindow", "Настенный календарь: " + str(self.count_photo["п_Настенный календарь_"])))
         self.label_magnit.setText(_translate("MainWindow", "магнит: " + str(self.count_photo["п_магнит_"])))
-        self.label_magnit_10x15.setText
-        (_translate("MainWindow", "магнит 10х15: " + str(self.count_photo["п_магнит 10х15_"])))
-        self.label_brelok58.setText(_translate("MainWindow", "Брелок 58: " + str(self.count_photo["Брелок 58"])))
-        self.label_zerkalo75.setText(_translate("MainWindow", "Зеркало 75: " + str(self.count_photo["Зеркало 75"])))
-        self.label_znaciok158.setText(_translate("MainWindow", "Значок 158: " + str(self.count_photo["Значок 158"])))
-        self.label_kopilka158.setText(_translate("MainWindow", "Копилка 158: " + str(self.count_photo["Копилка 158"])))
-        self.label_Cup.setText
-        (_translate("MainWindow", "Кружка-термос с крышкой " + str(self.count_photo["Кружка-термос с крышкой"])))
+        self.label_magnit_10x15.setText(_translate("MainWindow", "магнит 10х15: " + str(self.count_photo[
+                                                   "п_магнит 10х15_"])))
+        self.label_brelok58.setText(_translate("MainWindow", "Брелок 58: " + str(self.count_photo["_Брелок 58_"])))
+        self.label_zerkalo75.setText(_translate("MainWindow", "Зеркало 75: " + str(self.count_photo["_Зеркало 75_"])))
+        self.label_znaciok158.setText(_translate("MainWindow", "Значок 158: " + str(self.count_photo["_Значок 158_"])))
+        self.label_kopilka158.setText(_translate("MainWindow", "Копилка 158: " + str(self.count_photo[
+                                                 "_Копилка 158_"])))
+        self.label_Cup.setText(_translate("MainWindow", "Кружка-термос с крышкой: " + str(self.count_photo[
+                                          "_Кружка-термос с крышкой_"])))
 
     def add_psd_files_to_table(self, file, file_object, directory):
         """Функция добавления файла, имени обьекта и директории в таблицу.
@@ -206,15 +99,15 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
 
     def add_file_from_folder(self):
         """Добавляем список psd файлов в таблицу"""
+        self.zeroing_count_photo()
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
-        list_of_psd_files = combinator.search_file("psd", directory)
+        list_of_psd_files = search_file("psd", directory)
         self.tableWidget.clearContents()
         self.row_count = 0
         file_object = os.path.split(directory)[1]  # имя объекта к которому принадлежит файл
         for file in list_of_psd_files:
             self.add_psd_files_to_table(file, file_object, directory)
         self.tableWidget.resizeColumnsToContents()
-        self.zeroing_count_photo()
 
     def add_file(self):
         dialogObject = QtWidgets.QFileDialog(self, "Добавить файл", "", "Файлы изображений (*.psd)")
@@ -229,16 +122,17 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
             self.add_psd_files_to_table(file_name, file_object, directory)
         self.tableWidget.resizeColumnsToContents()
 
+
     def create_object_dir(self, dict_of_psd_files):
         """Создаем отдельные папки для каждого обьекта
         чтобы сконвертировать туда png. Основная папка
         описана в переменной dir_for_png"""
         try:
-            os.mkdir(dir_for_png)
+            os.mkdir(self.dir_for_png)
         except Exception as e:
-            print(e)
+            pass
         for k, v in dict_of_psd_files.items():
-            directory_object = dir_for_png + '\\' + os.path.split(k)[1]
+            directory_object = self.dir_for_png + '\\' + os.path.split(k)[1]
             try:
                 os.mkdir(directory_object)
             except Exception as e:
@@ -260,12 +154,6 @@ class MainApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
             count_row += 1
         progress_bar_maximum = count_row  # получаем количество фотографий для индикатора хода процесса
         return dict_of_psd_file
-
-    def compose_files(self):
-        global dict_of_psd_files
-        dict_of_psd_files = self.get_list_of_psd_from_table()
-        self.create_object_dir(dict_of_psd_files)
-        self.convert_psd_to_png()
 
 
 def main():
